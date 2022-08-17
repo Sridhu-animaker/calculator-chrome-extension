c4e0cce0d1b46ac6687d7da3bbf37891b93fc54f
Sridhu-animaker <sridhu@animaker.com>
2022-08-17 16:30:58 +0530

    second commit

--- public/background.js
+++ public/background.js
@@ -1,7 +1,5 @@
 /* global chrome */
-// chrome.runtime.onInstalled.addListener(() => {
-//   console.log(`Chrome extension added...`);
-// });
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     
   if (request.function === "numClickHandler") {
